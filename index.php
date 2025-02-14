f494237714d467bace93a96a01ce84ec2e810a43
monacocnm <monacocristian@gmail.com>
2025-02-14 20:21:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,144 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tabelas Chopp</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+
+<body>
+<div class="content">
+    <table class="rTable">
+        <thead>
+            <tr>
+                <th>ESTILO</th>
+                <th>CERVEJARIA</th>
+                <th>ABV %</th>
+                <th>IBU</th>
+				<th>CANECA</th>
+				<th>DESCART. 770ML</th>
+				<th>JARRA LT.</th>
+				<th>GROWLER LT.</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>IPA</td>
+                <td>Mônaco beer</td>
+                <td>6,0</td>
+                <td>65</td>
+				<td>R$ 13,00</td>
+				<td>R$ 26,00</td>
+				<td>R$ 30,00</td>
+				<td>R$ 30,00</td>
+            </tr>
+
+            <tr>
+                <td>WEISS</td>
+                <td>cerv.da serra</td>
+                <td>4,8</td>
+                <td>15</td>
+				<td>R$ 12,00</td>
+				<td>R$ 24,00</td>
+				<td>R$ 28,00</td>
+				<td>R$ 28,00</td> 
+            </tr>
+			
+			<tr>
+                <td>LAGER</td>
+                <td>Mônaco beer</td>
+                <td>4,5</td>
+                <td>12</td>
+				<td>R$ 10,00</td>
+				<td>R$ 16,00</td>
+				<td>R$ 25,00</td>
+				<td>R$ -----</td>
+            </tr>
+			
+			<tr>
+                <td>SESSION IPA</td>
+                <td>Mônaco beer</td>
+                <td>5,2</td>
+                <td>36</td>
+				<td>R$ 12,00</td>
+				<td>R$ 24,00</td>
+				<td>R$ 28,00</td>
+				<td>R$ 28,00</td>
+			</tr>
+			
+			<tr>
+                <td>BLACK</td>
+                <td>Mônaco beer</td>
+                <td>4,5</td>
+                <td>12</td>
+				<td>R$ 10,00</td>
+				<td>R$ 16,00</td>
+				<td>R$ 25,00</td>
+				<td>R$ -----</td> 
+
+            </tr>
+			
+			<tr> 
+                <td>PORTER</td>
+                <td>Mônaco Beer</td>
+                <td>5,0</td>
+                <td>25</td>
+				<td>R$ 12,00</td>
+				<td>R$ 24,00</td>
+				<td>R$ 28,00</td>
+				<td>R$ 28,00</td>  
+            </tr>
+			
+			<tr>
+                <td>SOUR MARACUJÁ</td>
+                <td>Berggren</td>
+                <td>4,0</td>
+                <td>5</td>
+				<td>R$ 14,00</td>
+				<td>R$ 26,00</td>
+				<td>R$ 30,00</td>
+				<td>R$ 30,00</td>
+            </tr>
+			
+<tr>   
+                <td>RED ALE</td>
+                <td>Quinta do Malte</td>
+                <td>5,0</td>
+                <td>25</td>
+				<td>R$ 10,00</td>
+				<td>R$ 20,00</td>
+				<td>R$ 28,00</td>
+				<td>R$ 28,00</td>
+            </tr>
+			
+			<tr>   
+                <td>VINHO</td>
+                <td>Villa Alemã</td>
+                <td>7,0</td>
+                <td>0</td>
+				<td>R$ 12,00</td>
+				<td>R$ 14,00</td>
+				<td>R$ 24,00</td>
+				<td>R$ 28,00</td>
+            </tr>
+			
+			<tr>
+                
+                <td>BLACK</td>
+                <td>Mônaco beer</td>
+                <td>4,5</td>
+                <td>12</td>
+				<td>R$ 10,00</td>
+				<td>R$ 12,00</td>
+				<td>R$ 16,00</td>
+				<td>R$ 20,00</td>
+            </tr>
+						
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
